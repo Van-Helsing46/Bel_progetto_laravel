387ecc88fbd13d95c4d44f3c59b7a6948a252091
Ettore <ettorecasamento97@gmail.com>
2022-02-18 22:08:46 +0100

    aggiunto commento simpatico in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -101,4 +101,6 @@ class UserController extends Controller
             return "ERRORE".$e->getFile().$e->getMessage().$e->getLine();
         }
     }
+
+    //questo commento è in realtà una funzione
 }
